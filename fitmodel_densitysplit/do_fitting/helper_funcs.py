a0ed85eec80b3c90381089cb77dc75e2f14c3b4e
Julian Wack <jwack@cuillin.roe.ac.uk>
2022-08-04 12:13:37 +0100

    more descriptive naming

---
+++ fitmodel_densitysplit/do_fitting/helper_funcs.py
@@ -0,0 +1,165 @@
+# Helper functions to compute the inverse covariance matrix. Functions allow to compute P(k,mu) for linear (or modified) Kaiser law.
+import numpy as np
+from scipy.special import legendre
+from scipy.integrate import simpson
+from scipy.linalg import LinAlgError, inv, pinv
+import scipy.sparse as ss
+
+from nbodykit.lab import FFTPower
+
+
+
+### Finding data P(k,mu) and multipoles ###
+def load_power_data(base_path, ells, get_data_Pkmus=True):
+    '''Loads in pre-computed power spectra for all density bins to find multipoles for each bin.
+    The base path specifies the location of the folder containing the density bin edges and the power spectra for each bin.
+    Optionally returns data based P(k,mu) and associated mu bins according to
+    eq 24 of Grieb et al. 2016: https://arxiv.org/pdf/1509.04293.pdf.
+    
+    We only need to subtract the shotnoise from the monopole due to the orthogonality of the Legendre polynomials.
+    As the shotnoise is constant wrt to $\mu$, it is proportional to the 0th Legendre polynomial s.t. only the monopole is affected.
+    '''
+    ptile_split = np.loadtxt(base_path+'power_densitybins/percentile_edges.txt')
+    n_ptile = len(ptile_split)-1 # number of bins = number of edges - 1
+    
+    # need number of k and mu bins to initilize arrays.
+    aux = FFTPower.load(base_path+'power_densitybins/ptile_0.json')
+    k = aux.poles['k']
+    mus = aux.power.coords['mu']
+    shotnoise = aux.attrs['shotnoise']
+    
+    Pk_ells = np.empty((n_ptile, len(ells), len(k))) # for each denisty bin store all multipoles
+    Pkmus = np.empty((n_ptile, len(k), len(mus))) # for each density bin store 2D power spectrum 
+
+    for i in range(n_ptile):
+        r = FFTPower.load(base_path+'power_densitybins/ptile_%d.json'%i)
+        poles = r.poles 
+
+        Pkmu_nl = np.zeros((len(k), len(mus)))
+        for j,ell in enumerate(ells):
+            Pk_ell = poles['power_%d' %ell].real
+            if ell == 0: 
+                Pk_ell = Pk_ell - poles.attrs['shotnoise']
+
+            Pk_ells[i][j] = Pk_ell
+            Pkmu_nl += np.outer(Pk_ell, legendre(ell)(mus))
+
+        Pkmus[i] = Pkmu_nl
+    
+    if get_data_Pkmus:
+        return k, shotnoise, n_ptile, Pk_ells, mus, Pkmus 
+    else:
+        return k, shotnoise, n_ptile, Pk_ells
+
+    
+
+### Finding model P(k,mu) and multipoles ###
+def kaiser_pkmu(k, mu, b1, beta, Plin):
+    """Returns power spectrum in redshift space, following Kaiser's linear results."""
+    return (1 + beta*mu**2)**2 *b1**2 * Plin(k)
+
+
+def fog_kaiser_pkmu(k, mu, b1, beta, sigma, Plin):
+    '''Introduces phenomenological Finger of God term to Kaiser model.
+    Common forms of this correction term are Gaussian or Lorentzian, both bring one new free parameter sigma to the model.
+    See eq 7.2 - 7.4 in Hamilton https://arxiv.org/pdf/astro-ph/9708102.pdf.'''
+    return np.exp(-0.5*(sigma*k*mu)**2)*kaiser_pkmu(k, mu, b1, beta, Plin)
+
+
+def make_Pkmu(k, b1, beta, sigma, Plin):
+    '''Make 2D array containing the model P(k,mu) with rows iterating k bins and columns iterating mu bins.
+    Later seek to integrate up each row i.e. integrate over mu. Nmu defines the discretiation.
+    Plin is function to compute the linear matter power spectrum such as nbodykit.cosmology.power.linear.LinearPower.'''
+    Nmu = 51
+    mus = np.linspace(-1,1,Nmu)
+    Pkmu = np.empty((len(k), Nmu))
+    
+    for i,mu in enumerate(mus):
+        Pkmu[:,i] = fog_kaiser_pkmu(k,mu,b1,beta,sigma,Plin)
+        
+    return Pkmu, mus
+
+
+def model_multipole(k, ell, b1, beta, sigma, Plin):
+    '''Computes ell-th multipole of damped Kaiser model P(k,mu) by projecting on Legendre polynominal.'''
+    Pkmu, mus = make_Pkmu(k, b1, beta, sigma, Plin)
+    L_ell = legendre(ell)(mus)
+    integrand = L_ell*Pkmu # each column gets multiplied by value of Legendre poly at assocaiated mu
+
+    return (2*ell+1)/2 * simpson(integrand, mus) # integrate up all rows separtely
+
+    
+
+### Finding inverse of analytic covariance matrix ###
+def per_mode_cov(k, l1, l2, BoxSize, shotnoise, dk, Pkmu, mus):
+    '''Construct per mode covariance. See eq 15, 16 (for factor f) of Grieb et al. 2016: https://arxiv.org/pdf/1509.04293.pdf
+    Pkmu is a 2D array of shape (# k bins, # mu bins). The above paper presents two possible choices in eq 23, 24 with the former 
+    being based on the model and later based on the data.'''
+    V = BoxSize**3
+    V_k = 4/3*np.pi*((k+dk/2)**3 - (k-dk/2)**3)
+    f = 2*(2*np.pi)**4 / V_k**2 * k**2 * dk
+    
+    L_l1, L_l2 = legendre(l1)(mus), legendre(l2)(mus)
+    integrand = (Pkmu + shotnoise)**2 * L_l1*L_l2
+    
+    return f*(2*l1+1)**2 * (2*l2+1)**2 / V * simpson(integrand, mus) # 1D array containing per mode cov for each k bin
+
+
+def gaussian_cov_mat_inv(k, ells, BoxSize, shotnoise, dk, Pkmu, mus):
+    '''See Grieb et al. 2016: https://arxiv.org/pdf/1509.04293.pdf or Fitting_b1_different_kmax.ipynb for explanation of structure of 
+    covariance matrix. Uses sparse matricies for fast inversion.
+    scipy.sparse.bmat allows to combine matricies by passing structure of larger matrix in terms of submatricies.'''
+    # initialize array accepting matricies as elements and fill with diagonal C_l1,l2 matricies
+    C = np.empty((len(ells), len(ells)), dtype='object')
+    for i,l1 in enumerate(ells):
+        for j,l2 in list(enumerate(ells))[i:]:
+            C[i][j] = ss.diags(per_mode_cov(k,l1,l2,BoxSize,shotnoise,dk,Pkmu,mus))
+            if j!=i:
+                C[j][i] = C[i][j]
+                
+    cov_mat = ss.bmat(C).tocsc() # convert to efficient scipy matrix format
+    
+    # deal with inverting signular matrix
+    try: 
+        inverse = ss.linalg.inv(cov_mat).toarray()
+    except LinAlgError or RuntimeError:
+        inverse = pinv(cov_mat.toarray())
+        
+    return inverse
+
+
+
+### Finding inverse of mock covariance matrix ###
+def slice_covmat(cov_mat, k_full, kmax):
+    '''Slices down full covariance matrix to k<=kmax<2 under the assumption that ells = [0,2].
+    The first k bin will be also be sliced away as the quadrupole vanishes in that bin, leading to a divide by 0 error 
+    when computing the correlation matrix.'''    
+    k_slice = (k_full <= kmax)
+    k_slice[0] = False # remove problematic first bin for fitting analysis
+    new_size = int(2*np.sum(k_slice)) # slices matrix will be of shape (new_size, new_size) 
+    mask = np.concatenate((k_slice, k_slice)) # when ells = [0,2,4] concatenate 3 times
+    mat_mask = np.outer(mask, mask) 
+    
+    return np.reshape(cov_mat[mat_mask], (new_size, new_size))
+
+
+def mock_cov_mat_inv(cov_mat, k, kmax):
+    '''Returns inverse of covariance matrix determined by brute force from many N body realizations. 
+    Covariance matricies for every density bin are stored on disk and computed for k<2. For the analysis at hand
+    we are often interested in a smaller kmax such that we need the slice the covariance matrix down first.
+    Note that the number of k bins from the mock boxes used for the covariance matrix must be the same as a the number of k bins
+    in the measurment box .i.e need to pass the same k_min, k_max, dk to FFTPower. The full set of the so obtained k values is k_full.
+    kmax is the maximum k until we seek to fit.
+    
+    Further note that the boxes used to get the covariance matrix have a BoxSize which is 4 times smaller than the BoxSize 
+    of the measurment simulation (500 vs 2000 Mpc/h). To account for this, divide covariance matrix by 4^3.'''
+    
+    sliced_covmat = slice_covmat(cov_mat, k, kmax) / 4**3
+    
+    # deal with inverting signular matrix
+    try: 
+        inverse = inv(sliced_covmat)
+    except LinAlgError or RuntimeError:
+        inverse = pinv(sliced_covmat)
+        
+    return inverse
---
+++ fitmodel_densitysplit/do_fitting/modelfit.py
@@ -0,0 +1,226 @@
+# Fit Kaiser with FoG term as explained in modelfit.ipynb
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.special import legendre, erf
+from datetime import timedelta
+import time
+from os import mkdir, listdir
+
+from astropy.cosmology import Planck18 as Planck18_astropy 
+import helper_funcs as hf
+import zeus 
+from nbodykit.lab import *
+
+
+### MCMC functions###
+def logprior(theta, i, kmax):
+    ''' The natural logarithm of the prior probability. Assume parameters independent such that log priors add.
+    Note that normalization is irrelevant for MCMC.'''
+    lp = 0.
+    b1, beta, sigma = theta
+    
+    sigma_min, sigma_max = 1, 5
+    sigma_max = 5 if kmax < 0.075 else 60
+    lp_sigma = 0. if sigma_min < sigma < sigma_max else -np.inf
+        
+    b1_min, b1_max = 0, 3
+    if i == 0:
+        beta_min, beta_max = -3, 3
+    else:
+        beta_min, beta_max = 0, 3
+        
+    lp_b1 = 0. if b1_min < b1 < b1_max else -np.inf
+    lp_beta = 0. if beta_min < beta < beta_max else -np.inf
+    
+    return lp_b1 + lp_beta + lp_sigma
+
+
+def loglike(theta, data_multipoles, k, C_inv):
+    '''Return logarithm of likelihood i.e. -0.5*chi2.
+    data_multipoles must be an array of shape (len(ells), len(k)). theta is parameter vector: [b1, beta, sigma].'''
+    ells = [0,2]
+    model_multipoles = np.empty((len(ells), len(k)))
+
+    b1, beta, sigma = theta
+    model_multipoles[0] = ( 1/(2*(k*sigma)**5) * (np.sqrt(2*np.pi)*erf(k*sigma/np.sqrt(2))*(3*beta**2+(k*sigma)**4+2*beta*(k*sigma)**2) + 
+                                                np.exp(-0.5*(k*sigma)**2)*(-2*beta*(beta+2)*(k*sigma)**3-6*beta**2*k*sigma) ) ) * b1**2 * Plin(k)
+    model_multipoles[1] = ( -5/(4*(k*sigma)**7) * (np.sqrt(2*np.pi)*erf(k*sigma/np.sqrt(2))*(-45*beta**2+(k*sigma)**6+(2*beta-3)*(k*sigma)**4+3*(beta-6)*beta*(k*sigma)**2) + 
+                                                np.exp(-0.5*(k*sigma)**2)*((4*beta*(beta+2)+6)*(k*sigma)**5+12*beta*(2*beta+3)*(k*sigma)**3+90*beta**2*k*sigma) ) ) * b1**2 * Plin(k)
+
+    D_M = (data_multipoles - model_multipoles).flatten()
+    
+    return -0.5*D_M@(C_inv @ D_M)
+
+
+def logpost(theta, i, data_multipoles, k, C_inv):
+    '''Returns the logarithm of the posterior. By Bayes' theorem, this is just the sum of the log prior and log likelihood (up 
+    to a irrelavant constant).
+    Uses values for theta from pre-analysis step to inform prior
+    ''' 
+    return logprior(theta, i, k[-1]) + loglike(theta, data_multipoles, k, C_inv)
+#####################
+
+
+### Set up MCMC ###
+LOS = [0,0,1]
+redshift = 0.2
+BoxSize = 2000
+
+cosmo = cosmology.Cosmology.from_astropy(Planck18_astropy)
+Plin = cosmology.LinearPower(cosmo, redshift, transfer='CLASS')
+sigma8_lin = Plin.sigma_r(8)
+sigma8_true = 0.8111 
+f_true = cosmo.scale_independent_growth_rate(redshift)
+
+ptile_labels = [r'$0^{th}$', r'$1^{st}$', r'$2^{nd}$', r'$3^{rd}$', r'$4^{th}$', r'$5^{th}$', r'$6^{th}$', r'$7^{th}$', r'$8^{th}$', r'$9^{th}$']
+dk = 0.01
+ells = [0,2]
+
+# load computed power spectra to deduce multipoles in each bin and P(k,mu) from data
+k_full, shotnoise, n_ptile, Pk_ells_full = hf.load_power_data('/home/jwack/main/fitmodel_densitysplit/', ells, get_data_Pkmus=False)
+# for given BoxSize, k is NaN above 0.034
+possible_kmax = k_full[k_full<=0.343][1:] # ignore first k bin
+
+kmax_range = possible_kmax[:21]
+Nkmax = len(kmax_range)
+
+b1_fits, beta_fits, sigma_fits, delta_fs8 = np.full((n_ptile, Nkmax), np.nan), np.full((n_ptile, Nkmax), np.nan), np.full((n_ptile, Nkmax), np.nan), np.full((n_ptile, Nkmax), np.nan) 
+b1_stds, beta_stds, sigma_stds, delta_fs8_stds = np.full((n_ptile, Nkmax), np.nan), np.full((n_ptile, Nkmax), np.nan), np.full((n_ptile, Nkmax), np.nan), np.full((n_ptile, Nkmax), np.nan)
+reduced_chi2 = np.full((n_ptile, Nkmax), np.nan)
+
+nsteps = 2500
+ndim = 3
+nwalkers = 8 
+start_b1 = 0.5 + 1*np.random.random(nwalkers)
+start_beta = 0.5 + 1*np.random.random(nwalkers)
+start_sigma = 1 + 4*np.random.random(nwalkers) 
+start = np.column_stack([start_b1, start_beta, start_sigma])
+###################
+
+
+### Run MCMC ###
+root_path = '/home/jwack/main/fitmodel_densitysplit/fit_results/FoG/'
+print("Fitting up to kmax=%.3f"%kmax_range[-1])
+
+for i in range(n_ptile):
+    store_path = root_path+'chains_ptile%d/'%i
+    if 'chains_ptile%d'%i not in listdir(root_path):
+        mkdir(store_path)
+    cov_mat = np.loadtxt('/home/jwack/main/fitmodel_densitysplit/bruteforce_covmat/covariance_matricies/cov_ptile_%d.txt'%i)
+    t1 = time.time()
+    for j,kmax in enumerate(kmax_range):
+        if 'k%d.npy'%j in listdir(store_path):
+            continue
+        # slice up to increasingly large kmax and find delta_fs8 for each bin
+        mask = np.full(len(k_full), False)
+        mask = k_full <= kmax
+        mask[0] = False 
+        k_sliced = k_full[mask]
+        Pk_ells_i = Pk_ells_full[:,:,mask][i]
+        C_inv = hf.mock_cov_mat_inv(cov_mat, k_full, kmax)
+        
+        sampler = zeus.EnsembleSampler(nwalkers, ndim, logpost, maxiter=1e5, verbose=False, args=[i, Pk_ells_i, k_sliced, C_inv]) 
+        sampler.run_mcmc(start, nsteps)
+        
+        chain = sampler.get_chain(flat=True, discard=nsteps//2)
+        # save chain without burn-in
+        np.save(store_path+'k%d'%j, chain)
+        
+        b1_fits[i][j], b1_stds[i][j] = np.mean(chain[:,0]), np.std(chain[:,0]) 
+        # parameter space is sym about b1=0 for Kaiser model. To get non negative fs8 assure that b1 and beta have the same sign
+        if i == 0:
+            b1_fits[i][j] *= -1
+        beta_fits[i][j], beta_stds[i][j] = np.mean(chain[:,1]), np.std(chain[:,1])
+        delta_fs8[i][j] = 1 - sigma8_lin*(beta_fits[i][j]*b1_fits[i][j])/(f_true*sigma8_true)
+        delta_fs8_stds[i][j] = np.abs(sigma8_lin/(f_true*sigma8_true)*(beta_stds[i][j]*b1_fits[i][j]+beta_fits[i][j]*b1_stds[i][j]))
+        sigma_fits[i][j], sigma_stds[i][j] = np.mean(chain[:,2]), np.std(chain[:,2])
+        reduced_chi2[i][j] = -2*loglike([b1_fits[i][j], beta_fits[i][j], sigma_fits[i][j]], Pk_ells_i, k_sliced, C_inv) / (len(ells)*len(k_sliced)-ndim)
+
+            
+    t2 = time.time()
+    print('Fitted %d-th percentile in %s'%(i,str(timedelta(seconds=t2-t1))))
+################
+
+
+### Store fit result ###
+np.savetxt(root_path+'b1_fits.txt', b1_fits)
+np.savetxt(root_path+'b1_stds.txt', b1_stds)
+
+np.savetxt(root_path+'beta_fits.txt', beta_fits)
+np.savetxt(root_path+'beta_stds.txt', beta_stds)
+
+np.savetxt(root_path+'delta_fs8.txt', delta_fs8)
+np.savetxt(root_path+'delta_fs8_stds.txt', delta_fs8_stds)
+
+np.savetxt(root_path+'sigma_fits.txt', sigma_fits)
+np.savetxt(root_path+'sigma_stds.txt', sigma_stds)
+
+np.savetxt(root_path+'reduced_chi2.txt', reduced_chi2)
+########################
+
+
+### Make fs8 plot ###
+fig = plt.figure(figsize=(20,8))
+
+for i in range(n_ptile):
+    plt.plot(kmax_range, delta_fs8[i], label=ptile_labels[i])
+    plt.fill_between(kmax_range, delta_fs8[i]-delta_fs8_stds[i,:], delta_fs8[i]+delta_fs8_stds[i,:], alpha=0.1)
+    
+# add 10% diviation lines
+plt.hlines([-0.1, 0.1], kmax_range[0], kmax_range[-1], linestyle='dotted', color='k', linewidth=2)
+
+plt.ylim([-0.3, 1.2])
+
+plt.title(r'relative difference to true $f(z)*\sigma_8(z)$ at $z=%.3f$'%redshift)
+plt.xlabel(r'$k_{max}$ [$h \ \mathrm{Mpc}^{-1}$]')
+plt.ylabel(r'$1 - \sigma_8^{lin}(z)*\beta*b_1/(f(z)\sigma_8(z))^{true}$')
+
+handles, labels = plt.gca().get_legend_handles_labels()
+fig.legend(handles, labels, loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=n_ptile)
+
+fig.savefig("/home/jwack/main/fitmodel_densitysplit/plots/KaiserFoG_dfs8_vs_kmax.pdf")
+#####################
+
+
+### Make fit plot ###
+fig = plt.figure(figsize=(26,18))
+
+ax_b1 = plt.subplot(2,3,1)
+ax_beta = plt.subplot(2,3,2)
+ax_sigma = plt.subplot(2,3,3)
+ax_chi2 = plt.subplot(2,3,(4,6))
+
+for i in range(n_ptile):
+    ax_b1.plot(kmax_range, b1_fits[i], label=ptile_labels[i])
+    ax_b1.fill_between(kmax_range, b1_fits[i]-b1_stds[i], b1_fits[i]+b1_stds[i], alpha=0.1)
+    
+    ax_beta.plot(kmax_range, beta_fits[i], label=ptile_labels[i])
+    ax_beta.fill_between(kmax_range, beta_fits[i]-beta_stds[i], beta_fits[i]+beta_stds[i], alpha=0.1)
+    
+    ax_sigma.plot(kmax_range, sigma_fits[i], label=ptile_labels[i])
+    ax_sigma.fill_between(kmax_range, sigma_fits[i]-sigma_stds[i], sigma_fits[i]+sigma_stds[i], alpha=0.1)
+    
+    ax_chi2.plot(kmax_range[1:], reduced_chi2[i][1:], label=ptile_labels[i]) # first element negative, s.t. not shown on log scale
+  
+ax_b1.set_title(r'$b_1$ mean and $1\sigma$ interval')
+ax_b1.set_xlabel(r'$k_{max}$ [$h \ \mathrm{Mpc}^{-1}$]')
+ax_b1.set_ylabel(r'$b_1$')
+
+ax_beta.set_title(r'$\beta$ mean and $1\sigma$ interval')
+ax_beta.set_xlabel(r'$k_{max}$ [$h \ \mathrm{Mpc}^{-1}$]')
+ax_beta.set_ylabel(r'$\beta$')
+
+ax_sigma.set_title(r'$\sigma$ mean and $1\sigma$ interval')
+ax_sigma.set_xlabel(r'$k_{max}$ [$h \ \mathrm{Mpc}^{-1}$]')
+ax_sigma.set_ylabel(r'$\sigma$ [$h^{-1} \ \mathrm{Mpc}$]')
+
+ax_chi2.set_title(r'reduced $\chi^2$')
+ax_chi2.set_yscale('log')
+ax_chi2.set_xlabel(r'$k_{max}$ [$h \ \mathrm{Mpc}^{-1}$]')
+ax_chi2.set_ylabel(r'$\chi^2 / dof$')
+
+handles, labels = plt.gca().get_legend_handles_labels()
+fig.legend(handles, labels, loc='upper center', bbox_to_anchor=(0.5, +0.05), ncol=n_ptile)
+
+fig.savefig("/home/jwack/main/fitmodel_densitysplit/plots/KaiserFoG_fits.pdf")
+#####################
